c364ef41199a5ed1c328b89b433a18e22c784300
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-12-14 20:34:23 +0300

    successful first migration

--- mainpage/settings.py
+++ mainpage/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'investment.apps.InvestmentConfig',
     'bootstrap4',
+    'rest_framework',
+   
+    'cloudinary',
+    'crispy_forms',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'investment.CustomUser'
